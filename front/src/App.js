3fd3063c3bb7c26258feb491106ad1d257bd19c7
Nikolay Panyukov <panyukovnn@gmail.com>
2022-03-30 23:39:52 +0300

    Добавлены заготовки главной страницы со списком статей, навигацоинной панели и элемента списка статей Добавлены страницы ошибок 404 и 500

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {Main} from "./pages/Main";
+import 'bootstrap/dist/css/bootstrap.css';
+import {NotFoundPage} from "./pages/exception/NotFoundPage";
+import {NavbarComponent} from "./components/NavbarComponent";
+import {NetworkErrorPage} from "./pages/exception/NetworkErrorPage";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavbarComponent/>
+
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Main/>}/>
+
+                    <Route path="*" element={<NotFoundPage/>}/>
+                    <Route path="/network-error" element={<NetworkErrorPage/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
