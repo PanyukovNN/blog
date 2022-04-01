33f9ddba5e8945bda0102912884f53a6ae6b16fb
Заводин Александр <aleksandr.zavodin@gazprombank.ru>
2022-04-02 00:44:30 +0300

    implemented createOrUpdate

--- back/src/main/java/org/example/blog/controller/ArticleController.java
+++ back/src/main/java/org/example/blog/controller/ArticleController.java
@@ -3,7 +3,9 @@ package org.example.blog.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.blog.dto.CreateArticleRequest;
 import org.example.blog.persistence.dto.ArticleDto;
+import org.example.blog.persistence.entity.Article;
 import org.example.blog.persistence.repository.ArticleRepository;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,8 +25,20 @@ public class ArticleController {
     }
 
     @PostMapping(value = "/create-update/{id}")
-    public void createOrUpdate(@PathVariable(required = false) String id, CreateArticleRequest createArticleDto) {
+    public ArticleDto createOrUpdate(@PathVariable(required = false) String id,
+                                     CreateArticleRequest createArticleDto) {
+        Article article = null;
 
+        if (StringUtils.hasText(id)) {
+            article = articleRepository.findById(id).get();
+        } else {
+            article = new Article();
+        }
+
+        article.setHeader(createArticleDto.getTitle());
+        article.setContent(createArticleDto.getContent());
+
+        return new ArticleDto(article);
     }
 
     @DeleteMapping(value = "/delete/{id}")
