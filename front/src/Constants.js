769332fea27e7e5104d91fb0f10a93d96d9ce77c
Nikolay Panyukov <panyukovnn@gmail.com>
2022-04-04 23:32:51 +0300

    Добавлен RequestService.js

---
+++ front/src/Constants.js
@@ -0,0 +1,5 @@
+
+/**
+ * Ссылки
+ */
+export const BACK_URL = "http://localhost:8080/api"
---
+++ front/src/service/RequestService.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+export function postReq(url, body) {
+    let postResponse = axios.post(url,
+        JSON.stringify(body),
+        {
+            headers : {'Content-Type': 'application/json'}
+        });
+
+    return processResponse(postResponse);
+}
+
+export function getReq(url) {
+    let getResponse = axios.get(url,
+        {
+            headers : {'Content-Type': 'application/json'}
+        });
+
+    return processResponse(getResponse);
+}
+
+export function deleteReq(url, body) {
+    let deleteResponse = axios.delete(url,{
+        data: JSON.stringify(body),
+        headers : {'Content-Type': 'application/json'}
+    });
+
+    return processResponse(deleteResponse);
+}
+
+const processResponse = (axiosResponse) => {
+    return axiosResponse
+        .then((response) => {
+            if (response.status !== 200) {
+                throw response;
+            }
+
+            return response;
+        })
+        .catch((error) => {
+            if (error.response && error.response.data && error.response.message) {
+                showAlert(error.response.data.message);
+            } else {
+                if (error.message && error.message === "Network Error") {
+                    window.location.href = "/network-error";
+
+                    return;
+                }
+
+                showAlert(error.response.data);
+            }
+        });
+}
