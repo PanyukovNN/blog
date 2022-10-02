11c90e7b78e0611f13b9bc8969350b7aadab32c8
Nikolay Panyukov <panyukovnn@gmail.com>
2022-10-02 18:08:02 +0300

    Feature: add exception handling

---
+++ back/src/main/java/org/reactivetales/blog/model/response/ErrorResponse.java
@@ -0,0 +1,12 @@
+package org.reactivetales.blog.model.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ErrorResponse {
+
+    private int status;
+    private String message;
+}
